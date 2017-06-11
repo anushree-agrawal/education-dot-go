f438ecd0655a07efc07f6718d6ec9e9975e44964
Joseph Gao <gaoj@seas.upenn.edu>
2017-06-10 22:44:02 -0700

    added orm stuff

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
+import datetime
+from peewee import *
+from playhouse.flask_utils import FlaskDB
+
+DATABASE = ''
+
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+db_wrapper = FlaskDB(app)
+
+
+# add schemas below
+class User(db_wrapper.Model):
+    # username = CharField(unique=True)
+
+class Tweet(db_wrapper.Model):
+    # user = ForeignKeyField(User, related_name='tweets')
+    # content = TextField()
+    # timestamp = DateTimeField(default=datetime.datetime.now)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
